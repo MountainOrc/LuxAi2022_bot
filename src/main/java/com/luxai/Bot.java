0c553efdfc261688d1f0d7f415d67e54f2950451
Alexey Pogasiy <8132209-apogasiy@users.noreply.gitlab.com>
2022-11-29 15:29:58 +0300

    Agent act() refactoring

--- src/main/java/com/luxai/Bot.java
+++ src/main/java/com/luxai/Bot.java
@@ -7,13 +7,12 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Main class
+ * Lux-ai-2022
  */
 public class Bot
 {
